b6565c5fbf7ad4096ec273d21f8726170a012403
tkhodges <thodges@som.umaryland.edu>
2017-04-30 12:46:00 -0400

    Completed Programming Assignment#2
    Here is my submission for the programming assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Programming Assignment#2 - Caching the Inverse of a Matrix
+##For the assignment, a pair of functions was written to cache the inverse of a matrix
 
-## Write a short comment describing this function
-
+##makeCacheMatrix function creates a special "matrix" object that can cache its inverse. The function builds a set of functions and returns the functions within a list
 makeCacheMatrix <- function(x = matrix()) {
+          oMat <- NULL
+          set <- function(y){
+                        x <<- y
+                        oMat <<- NULL
+          }
+          get <- function() x
+          set_matrix <- function(inverse) oMat <<- inverse
+          get_inverse <- function() oMat
+          list(set = set, get = get,
+               set_matrix = set_matrix,
+               get_inverse = get_inverse)
+          
+            }
+  
 
-}
-
-
-## Write a short comment describing this function
+##cacheSolve function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+          oMat <- x$get_inverse()
+          if(!is.null(oMat)) {
+          message("getting cached data")
+          return(oMat)
+          }
+          my_matrix <- x$get()
+          oMat <- solve(my_matrix, ...)
+          x$set_matrix(oMat)
+          oMat
 }
+
+
+
+
+
+
